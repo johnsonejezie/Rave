dc377a78dba8fcb1b49ecc5df293ade07f992940
Johnson Ejezie <johnsonejezie@gmail.com>
2017-01-29 11:51:19 +0100

    Initial Commit

---
+++ Rave/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rave
+//
+//  Created by Johnson Ejezie on 29/01/2017.
+//  Copyright © 2017 johnsonejezie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
